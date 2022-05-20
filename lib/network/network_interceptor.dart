64367f70b98164c8fea537adf7ae0bfe09b548f6
jinfeng <851406283@qq.com>
2022-05-20 15:02:16 +0800

    网络库第一次提交 ‘

---
+++ lib/network/network_interceptor.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+
+class NetworkInterceptor extends Interceptor {
+  // @override
+  // void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+  //   super.onRequest(options, handler);
+  // }
+
+  // @override
+  // void onResponse(dio.Response response, ResponseInterceptorHandler handler) {
+  //   super.onResponse(response, handler);
+  // }
+}
